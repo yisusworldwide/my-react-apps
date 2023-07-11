c0ca3b221b2c65dda23dcdb1c551d85d6a1ed698
yisusworldwide <jesusjimenez.email@gmail.com>
2023-07-11 09:29:54 +0200

    new Clicker alert message exercise

--- src/Clicker.jsx
+++ src/Clicker.jsx
@@ -1,15 +1,6 @@
-function handleClick(){
-    console.log("Button Clicked!");
-}
-
-export default function Clicker(){
-
-    return (
-        <div>
-            <p>Click the button down below:</p>
-            <button onClick={handleClick}>Click here!!</button>
-        </div>
-        
-    );
+export default function Clicker({message, buttonText}){
+    const handleClick = ()=>{
+        alert(message);
+    };
 
 }
