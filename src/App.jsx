e1b75c459df75ee631c8f3612842ea875c596689
yisusworldwide <jesusjimenez.email@gmail.com>
2023-07-15 20:34:09 +0200

    Removed original count imported from useState to leave app neat

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react'
 import './App.css'
 import Clicker from './Clicker.jsx'
 import Form from './Form.jsx'
 
 function App() {
-  //const [count, setCount] = useState(0)
 
   return (
     <>
@@ -15,13 +13,6 @@ function App() {
         <Clicker message="touch me again baby!" buttonText="horny here"/>
       </div>
 
-
-      {/* <div className="card">
-        
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div> */}
     </>
   )
 }
