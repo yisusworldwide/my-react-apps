7cbd1717ce17446957efab44ea003cb6b3fd02ff
yisusworldwide <jesusjimenez.email@gmail.com>
2023-08-06 20:44:58 +0200

    Added "p.id" as key for PropertyList component

--- src/PropertyList.jsx
+++ src/PropertyList.jsx
@@ -5,7 +5,7 @@ export default function PropertyList({properties}){
     return (
         <div className = "PropertyList">
             {properties.map((p) => {
-                return <Property {...p} />
+                return <Property {...p} key={p.id} />
             })}
         </div>
     );
