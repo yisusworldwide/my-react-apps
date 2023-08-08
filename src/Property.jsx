1d2545f32e28c4a9eb74a2ce21c50071c278ef90
yisusworldwide <jesusjimenez.email@gmail.com>
2023-08-08 10:58:24 +0200

    No className for "Property.jsx" and final fixings

--- src/Property.jsx
+++ src/Property.jsx
@@ -1,6 +1,6 @@
 export default function Property({ name, price, rating, id }){
     return (
-        <div className="Property">
+        <div>
             <h2>{name}</h2>
             <h3>${price} per night</h3>
             <h4>{rating} stars</h4>
