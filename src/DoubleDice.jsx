43c280c4840c955c6869a073b18794e0fb4f268e
yisusworldwide <jesusjimenez.email@gmail.com>
2023-08-08 20:42:55 +0200

    Plain react conditional

--- src/DoubleDice.jsx
+++ src/DoubleDice.jsx
@@ -1,9 +1,11 @@
 export default function DoubleDice (){
     const num1 = Math.floor(Math.random() * 3) +1;
     const num2 = Math.floor(Math.random() * 3) +1;
+    const result = num1 === num2 ? "You win!" : "You lose :(";
     
     return (
         <div>
+            <h2>{result}</h2>
             <p>Num1: {num1}</p>
             <p>Num2: {num2}</p>
         </div>
