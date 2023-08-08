caa69d203d74c0b238584accfc43a733a327d8d3
yisusworldwide <jesusjimenez.email@gmail.com>
2023-08-08 20:50:30 +0200

    Conditional in-line including h3 or null

--- src/DoubleDice.jsx
+++ src/DoubleDice.jsx
@@ -1,11 +1,11 @@
 export default function DoubleDice (){
     const num1 = Math.floor(Math.random() * 3) +1;
     const num2 = Math.floor(Math.random() * 3) +1;
-    const result = num1 === num2 ? "You win!" : "You lose :(";
     
     return (
         <div>
-            <h2>{result}</h2>
+            <h2>Double Dice</h2>
+            {num1 === num2 ? <h3>You win mfucker!</h3> : null}
             <p>Num1: {num1}</p>
             <p>Num2: {num2}</p>
         </div>
