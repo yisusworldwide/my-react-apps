2647fa0ee21badffe6043458253ae7c9141b5f44
yisusworldwide <jesusjimenez.email@gmail.com>
2023-08-09 14:34:27 +0200

    Conditional with ampersand on Blackjack adaptation

--- src/DoubleDice.jsx
+++ src/DoubleDice.jsx
@@ -1,13 +1,17 @@
 export default function DoubleDice (){
-    const num1 = Math.floor(Math.random() * 3) +1;
-    const num2 = Math.floor(Math.random() * 3) +1;
+    const num1 = Math.floor(Math.random() * 6) +1;
+    const num2 = Math.floor(Math.random() * 6) +1;
+    const num3 = Math.floor(Math.random() * 6) +1;
+    const num4 = Math.floor(Math.random() * 6) +1;
     
     return (
         <div>
             <h2>Double Dice</h2>
-            {num1 === num2 ? <h3>You win mfucker!</h3> : null}
+            {num1 + num2 + num3 + num4 === 21 && <h3>Blackjack!</h3>}
             <p>Num1: {num1}</p>
             <p>Num2: {num2}</p>
+            <p>Num3: {num3}</p>
+            <p>Num4: {num4}</p>
         </div>
     );
 }
