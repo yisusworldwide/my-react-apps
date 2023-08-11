24a3caf6fcdf8c60f2c0e936dc565a4fefd2a689
yisusworldwide <jesusjimenez.email@gmail.com>
2023-08-12 01:01:50 +0200

    Conditional styling with "isWinner" function

--- src/DoubleDice.jsx
+++ src/DoubleDice.jsx
@@ -3,13 +3,15 @@ export default function DoubleDice (){
     const num2 = Math.floor(Math.random() * 6) +1;
     const num3 = Math.floor(Math.random() * 6) +1;
     const num4 = Math.floor(Math.random() * 6) +1;
-    
-    const styles = {color: num1 + num2 + num3 + num4 === 21 ? "green" : "red"};
+
+    const isWinner = num1 + num2 + num3 + num4 === 21;
+    const styles = {color: isWinner ? "green" : "red"}
+
     return (
         
         <div style={styles}>
             <h2>Double Dice</h2>
-            {num1 + num2 + num3 + num4 === 21 && <h3>Blackjack!</h3>}
+            {num1 + num2 + num3 + num4 === 21 && <h3>Blackjack!!!</h3>}
             <p>Num1: {num1}</p>
             <p>Num2: {num2}</p>
             <p>Num3: {num3}</p>
