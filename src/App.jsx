1dbc952ee0f2e3418537ffa4cc3777e1b41d0d5b
yisusworldwide <jesusjimenez.email@gmail.com>
2023-08-31 10:00:36 +0200

    Added "FavoriteIcon" component in Property.jsx from Material UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import './App.css';
 import RatingDemo from './RatingDemo';
 import ScoreKeeper from "./ScoreKeeper";
-//import PropertyList from "./PropertyList";
+import PropertyList from "./PropertyList";
 
 function App() {
 
@@ -12,9 +12,9 @@ function App() {
         <ScoreKeeper numPlayers={5} target={7} />
       </div> */}
 
-      {/* <div>
+      <div>
         <PropertyList properties={properties} />
-      </div> */}
+      </div>
 
     </>
   );
@@ -22,14 +22,14 @@ function App() {
 
 export default App
 
-// const properties = [
-//   { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
-//   { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
-//   { id: 129032, name: "Cactus Retreat", rating: 4.75, price: 300 },
-//   { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
-//   { id: 129034, name: "Oceanview Condo", rating: 4.7, price: 140 },
-//   { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: 96 },
-// ];
+const properties = [
+  { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
+  { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
+  { id: 129032, name: "Cactus Retreat", rating: 4.75, price: 300 },
+  { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
+  { id: 129034, name: "Oceanview Condo", rating: 4.7, price: 140 },
+  { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: 96 },
+];
 
 // Array containing colors
 // const colors = [
--- src/Property.jsx
+++ src/Property.jsx
@@ -10,21 +10,29 @@
 
 import { useState } from "react";
 import Rating from '@mui/material/Rating';
+import Box from '@mui/material/Box';
+import FavoriteIcon from '@mui/icons-material/Favorite';
 
 export default function Property({ name, price, id }){
     const [value, setValue] = useState(3);
     return (
         <div>
-            <h2>{name}</h2>
-            <h3>${price} per night</h3>
-            <Rating
-                name="simple-controlled"
-                value={value}
-                onChange={(event, newValue) => {
-                setValue(newValue);
-                }}
-            />
-            
+            <Box sx={{ '& > :not(style)': { m: 1 } }}>
+                <h2>{name}</h2>
+                <h3>${price} per night</h3>
+                <p>
+                    <Rating
+                        name="simple-controlled"
+                        value={value}
+                        onChange={(event, newValue) => {
+                        setValue(newValue);
+                        }}
+                    />
+                </p>
+                <p disabled aria-label="like">
+                    <FavoriteIcon />
+                </p>
+            </Box>
         </div>
     );
 }
