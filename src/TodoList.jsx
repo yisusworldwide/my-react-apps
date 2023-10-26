dcc1cdb20f0277cff2f6904000ba944a3746ccd2
yisusworldwide <jesusjimenez.email@gmail.com>
2023-10-26 20:39:47 +0200

    Generated array "initialTodos" to loop them down in our component

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,3 +1,10 @@
+const initialTodos = [
+    {id: 1, text: "do the workout", completed: true},
+    {id: 3, text: "shake the sardin", completed: false},
+    {id: 5, text: "expel the poison", completed: true},
+    {id: 7, text: "go sleep early", completed: false},
+] 
+
 export default function TodoList(){
     return (
 
