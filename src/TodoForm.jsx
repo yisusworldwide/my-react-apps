3f17ead5e94a1c7290cd88678b57a9de5745e772
yisusworldwide <jesusjimenez.email@gmail.com>
2023-11-07 00:56:34 +0100

    Created "addTodo" for TodoForm in TodoList.jsx

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -6,19 +6,27 @@ import Create from "@mui/icons-material/Create";
 import IconButton from '@mui/material/IconButton';
 
 
-export default function TodoItem (){
+export default function TodoForm ({ addTodo }){
     const [text, setText] = useState("");
     const handleChange = (evt) => {
         setText(evt.target.value);
-    }
+    };
+    
+    const handleSubmit = (e) => {
+        //we prevent the form updating when clicking "enter"
+        e.preventDefault();
+        addTodo(text);
+        setText("");
+    };
+    
     return(
         <ListItem>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <TextField id="outlined-basic" label="Outlined" variant="outlined" onChange={handleChange} value={text} 
                     InputProps = {{
                         endAdornment:
                             <InputAdornment position="end">
-                            <IconButton aria-label="create todo" edge="end">
+                            <IconButton aria-label="create todo" edge="end" type="submit">
                                 <Create />
                             </IconButton>
                             </InputAdornment>
@@ -35,4 +43,4 @@ export default function TodoItem (){
             type={showPassword ? 'text' : 'password'}
             
             label="Password"
-          /> */}
+    /> */}
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -12,13 +12,14 @@ const initialTodos = [
 
 export default function TodoList(){
     const [todos, setTodos] = useState(initialTodos);
+    //funtion to REMOVE todos
     const removeTodo = (id) => {
       setTodos(prevTodos => {
         //we return all the todos which "id" are different from the one given as argument above
         return prevTodos.filter((t) => t.id != id);
       });
     };
-
+    //function to change from true to false and likewise 
     const toggleTodo = (id) => {
       setTodos(prevTodos => {
         return prevTodos.map(todo => {
@@ -32,18 +33,28 @@ export default function TodoList(){
       })
     }
 
+    const addTodo = (text) => {
+      setTodos ((prevTodos) => {
+
+        return [...prevTodos, {text:text, id:9, completed: false}];
+        }
+      );
+    }
+    //returns the original list giving the above hardcoded todos
     return (
         <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
             {todos.map((todo)=> ( 
                 <TodoItem 
                   todo={todo} 
                   key={todo.id}
+                  //try to remove item with this variable commented out
                   remove={removeTodo}
                   toggle={() => toggleTodo(todo.id)}
+                  // addTodo={addTodo} try to explain why here NOT working
                 />
           ) ) }
           
-          <TodoForm />
+          <TodoForm addTodo={addTodo}/> 
       
         </List>
     );
