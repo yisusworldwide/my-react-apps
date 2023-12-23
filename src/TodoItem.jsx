a65a88044b6a8b42ea6e4ee1a08ace0a25d0b481
yisusworldwide <jesusjimenez.email@gmail.com>
2023-12-23 20:50:32 +0100

    Removed "CommentIcon" from TodoItem.jsx

--- src/TodoItem.jsx
+++ src/TodoItem.jsx
@@ -4,7 +4,6 @@ import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
 import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
-//import CommentIcon from '@mui/icons-material/Comment';
 import DeleteIcon from '@mui/icons-material/Delete';
 
 export default function TodoItem ({todo, remove, toggle}){
